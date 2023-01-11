7ac05234285fe70a56fec1c202bc0c75d5ab0801
Your Name <you@example.com>
2023-01-11 11:45:27 +0700

    11/1/2023

--- StudentManager/src/App.java
+++ StudentManager/src/App.java
@@ -4,8 +4,19 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) {
+//        try {
+//            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//            new LoginView();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
             new LoginView();
         } catch (Exception e) {
             e.printStackTrace();
